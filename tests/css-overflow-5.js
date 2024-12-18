9095c4020cefd83b6f7f2717626cc6da1cd1ec8c
Sebastian Zartner <sebastianzartner@gmail.com>
2024-12-18 13:46:20 +0100

    Updated CSS Overflow 5 and added TR links

--- tests/css-overflow-5.js
+++ tests/css-overflow-5.js
@@ -1,6 +1,7 @@
 export default {
 	title: 'CSS Overflow Module Level 5',
 	links: {
+		tr: 'css-overflow-5',
 		dev: 'css-overflow-5',
 	},
 	status: {
@@ -9,7 +10,8 @@ export default {
 	properties: {
 		'scroll-marker-group': {
 			links: {
-				dev: '#scroll-marker-group',
+				tr: '#scroll-marker-group-property',
+				dev: '#scroll-marker-group-property',
 			},
 			tests: [
 				'none',
@@ -21,15 +23,24 @@ export default {
 	selectors: {
 		'::scroll-marker': {
 			links: {
+				tr: '#scroll-marker-pseudo',
 				dev: '#scroll-marker-pseudo',
 			},
 			tests: ['::scroll-marker'],
 		},
 		'::scroll-marker-group': {
 			links: {
+				tr: '#scroll-marker-group-pseudo',
 				dev: '#scroll-marker-group-pseudo',
 			},
 			tests: ['::scroll-marker-group'],
+		},
+		':target-current': {
+			links: {
+				tr: '#active-scroll-marker',
+				dev: '#active-scroll-marker',
+			},
+			tests: [':target-current'],
 		},
 	},
 };
