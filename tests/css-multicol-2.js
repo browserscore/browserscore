c08d0c642285d6a4f9eda782e7a9c280eb19bfb1
Sebastian Zartner <sebastianzartner@gmail.com>
2024-12-19 13:43:02 +0100

    Updated CSS Multicol 2 and added TR links

--- tests/css-multicol-2.js
+++ tests/css-multicol-2.js
@@ -1,6 +1,7 @@
 export default {
 	title: 'CSS Multi-column Layout Module Level 2',
 	links: {
+		tr: 'css-multicol-2',
 		dev: 'css-multicol-2',
 	},
 	status: {
@@ -9,9 +10,19 @@ export default {
 	properties: {
 		'column-span': {
 			links: {
+				tr: '#column-span',
 				dev: '#column-span',
 			},
 			tests: ['2', 'auto'],
 		},
 	},
+	selectors: {
+		'::column': {
+			links: {
+				tr: '#column-pseudo',
+				dev: '#column-pseudo',
+			},
+			tests: ['::column'],
+		},
+	}
 };
