4664cd3303742ccc42afe9eed1210fabe915d89b
Lea Verou <lea@verou.me>
2025-08-26 16:30:37 +0300

    `<support-status>`: Display warning icon if NaN

--- src/vue/components/support-status/support-status.js
+++ src/vue/components/support-status/support-status.js
@@ -3,14 +3,15 @@ import { round, passclass } from '../../../util.js';
 let css = fetch(new URL('./support-status.css', import.meta.url)).then(res => res.text());
 
 const template = `
-	<svg ref="el" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="progress" :class="passclass(score)" :style="{ '--progress': round(score.value * 100, 2) }">
-		<title>Passed {{ round(score.passed) }} / {{ round(score.total) }} tests in {{ round(score.testTime, 2) }} ms</title>
-
-		<circle v-if="score.value > 0" cx="50" cy="50" r="45" />
-		<polyline v-if="score.value >= 1" class="check" points="33,50 46,65 68,36" />
-		<use v-else-if="score.value === 0" href="ui/icons/x.svg#x" width="100%" height="100%" class="x" />
-		<circle v-else cx="50" cy="50" r="45" class="partial" />
-	</svg>`;
+<svg ref="el" v-if="!isNaN(score.value)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="progress" :class="passclass(score)" :style="{ '--progress': round(score.value * 100, 2) }">
+	<title>{{ tooltip }}</title>
+	<circle v-if="score.value > 0" cx="50" cy="50" r="45" />
+	<polyline v-if="score.value >= 1" class="check" points="33,50 46,65 68,36" />
+	<use v-else-if="score.value === 0" href="ui/icons/x.svg#x" width="100%" height="100%" class="x" />
+	<circle v-else cx="50" cy="50" r="45" class="partial" />
+</svg>
+<span v-else class="icon" style="--icon: var(--icon-warning)" :title="tooltip"></span>
+`;
 
 export default {
 	props: {
@@ -21,6 +22,12 @@ export default {
 	},
 
 	template,
+
+	computed: {
+		tooltip () {
+			return `Passed ${round(this.score.passed)} / ${round(this.score.total)} tests in ${round(this.score.testTime, 2)} ms`
+		}
+	},
 
 	methods: {
 		round,
