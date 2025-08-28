445f9213c57df42e8b32dd1d35aaa372ffd2fdd3
Lea Verou <lea@verou.me>
2025-08-28 04:58:06 +0300

    Do not render child tests until their `<details>` is opened
    This takes the HTML elements in the page on initial load down from 65K (!) to "just" 13.5K

--- src/vue/components/feature/feature.js
+++ src/vue/components/feature/feature.js
@@ -8,10 +8,20 @@ export default {
 		},
 	},
 
+	data () {
+		return {
+			open: false,
+			everOpened: false,
+			renderedChildren: []
+		};
+	},
+
 	mounted () {
+		let container = this.$refs.container ?? this.$refs.details;
+
 		if (IS_DEV) {
 			// Expose feature object on container for debugging
-			this.$refs.container.feature = this.feature;
+			container.feature = this.feature;
 		}
 	},
 
@@ -19,5 +29,25 @@ export default {
 
 	methods: {
 		passclass,
+
+		handleToggle (event) {
+			let open = event.target.open;
+			this.open = open;
+
+			if (open && !this.everOpened) {
+				this.everOpened = true;
+			}
+
+		},
 	},
+
+	watch: {
+		everOpened: {
+			handler (newVal) {
+				if (newVal) {
+					this.renderedChildren = this.feature.children;
+				}
+			},
+		},
+	}
 };
