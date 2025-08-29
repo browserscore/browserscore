50ec36ca541b98ba089b39fa4edeb5e5cfa27827
Lea Verou <lea@verou.me>
2025-08-29 18:48:46 +0300

    Start helpers for more systematic value generation and use them in `css2-box`

--- tests/css2-box.js
+++ tests/css2-box.js
@@ -1,3 +1,23 @@
+import { repeat, combine } from '../src/combine.js';
+
+const length = '1px';
+const percentage = '1%';
+const margin = [length, percentage, 'auto'];
+const margin_1_4 = repeat(margin, {min: 1, max: 4});
+
+const padding = [length, percentage];
+const padding_1_4 = repeat(padding, {min: 1, max: 4});
+
+const borderColors = ['red', 'transparent'];
+const borderStyles = ['solid', 'none', 'hidden', 'dotted', 'dashed', 'double', 'groove', 'ridge', 'inset', 'outset'];
+const borderWidths = [length, 'thin', 'medium', 'thick'];
+
+const borderColors_1_4 = repeat(borderColors, {min: 1, max: 4});
+const borderStyles_1_4 = repeat(borderStyles.slice(0, 2), {min: 1, max: 4});
+const borderWidths_1_4 = repeat(borderWidths.slice(0, 2), {min: 1, max: 4});
+
+const borderShorthands = combine([borderWidths[0], borderStyles[0], borderColors[0]], { combinator: '||' });
+
 export default {
 	id: 'css2-box',
 	title: 'CSS 2 Box Model',
@@ -9,165 +29,126 @@ export default {
 	firstSnapshot: 2.2,
 	lastSnapshot: 2.2,
 	properties: {
+		border: {
+			link: '#border-shorthand-properties',
+			tests: borderShorthands,
+		},
 		'border-color': {
 			link: '#border-color-properties',
-			tests: ['black', '#ff0000 rgb(0, 255, 0)', 'rgb(0%, 0%, 100%) #0f0 transparent', 'red green blue yellow'],
+			tests: borderColors_1_4,
 		},
 		'border-style': {
 			link: '#border-style-properties',
-			tests: ['none', 'hidden', 'none dashed', 'none dashed dotted', 'none dashed dotted solid'],
-		},
-		'border-top': {
-			link: '#border-shorthand-properties',
-			tests: [
-				'black',
-				'dotted',
-				'5px',
-				'#ff0000 dashed',
-				'solid 0.2em',
-				'rgb(0, 0, 255) 0.1ex',
-				'#0f0 double 0.8mm',
-			],
-		},
-		'border-right': {
-			link: '#border-shorthand-properties',
-			tests: [
-				'black',
-				'dotted',
-				'5px',
-				'#ff0000 dashed',
-				'solid 0.2em',
-				'rgb(0, 0, 255) 0.1ex',
-				'#0f0 double 0.8mm',
-			],
-		},
-		'border-bottom': {
-			link: '#border-shorthand-properties',
-			tests: [
-				'black',
-				'dotted',
-				'5px',
-				'#ff0000 dashed',
-				'solid 0.2em',
-				'rgb(0, 0, 255) 0.1ex',
-				'#0f0 double 0.8mm',
-			],
-		},
-		'border-left': {
-			link: '#border-shorthand-properties',
-			tests: [
-				'black',
-				'dotted',
-				'5px',
-				'#ff0000 dashed',
-				'solid 0.2em',
-				'rgb(0, 0, 255) 0.1ex',
-				'#0f0 double 0.8mm',
-			],
-		},
-		'border-top-color': {
-			link: '#border-color-properties',
-			tests: ['black', '#00f', '#000000', 'rgb(255, 255, 255)', 'rgb(100%, 50%, 50%)', 'transparent'],
-		},
-		'border-right-color': {
-			link: '#border-color-properties',
-			tests: ['black', '#00f', '#000000', 'rgb(255, 255, 255)', 'rgb(100%, 50%, 50%)', 'transparent'],
-		},
-		'border-bottom-color': {
-			link: '#border-color-properties',
-			tests: ['black', '#00f', '#000000', 'rgb(255, 255, 255)', 'rgb(100%, 50%, 50%)', 'transparent'],
-		},
-		'border-left-color': {
-			link: '#border-color-properties',
-			tests: ['black', '#00f', '#000000', 'rgb(255, 255, 255)', 'rgb(100%, 50%, 50%)', 'transparent'],
-		},
-		'border-top-style': {
-			link: '#border-style-properties',
-			tests: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset'],
-		},
-		'border-right-style': {
-			link: '#border-style-properties',
-			tests: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset'],
-		},
-		'border-bottom-style': {
-			link: '#border-style-properties',
-			tests: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset'],
-		},
-		'border-left-style': {
-			link: '#border-style-properties',
-			tests: ['none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset'],
-		},
-		'border-top-width': {
-			link: '#border-width-properties',
-			tests: ['thin', 'medium', 'thick', '5px'],
-		},
-		'border-right-width': {
-			link: '#border-width-properties',
-			tests: ['thin', 'medium', 'thick', '5px'],
-		},
-		'border-bottom-width': {
-			link: '#border-width-properties',
-			tests: ['thin', 'medium', 'thick', '5px'],
-		},
-		'border-left-width': {
-			link: '#border-width-properties',
-			tests: ['thin', 'medium', 'thick', '5px'],
+			tests: borderStyles_1_4,
 		},
 		'border-width': {
 			link: '#border-width-properties',
-			tests: ['thin', 'thin medium', 'thin medium thick', 'thin medium thick 5px'],
+			tests: borderWidths_1_4,
 		},
-		border: {
+		'border-top': {
 			link: '#border-shorthand-properties',
-			tests: [
-				'black',
-				'dotted',
-				'5px',
-				'#ff0000 dashed',
-				'solid 0.2em',
-				'rgb(0, 0, 255) 0.1ex',
-				'rgb(100%, 50%, 50%) double 0.8mm',
-			],
+			tests: borderShorthands,
 		},
-		'margin-right': {
-			link: '#propdef-margin-right',
-			tests: ['auto', '10px', '5%'],
+		'border-right': {
+			link: '#border-shorthand-properties',
+			tests: borderShorthands,
 		},
-		'margin-left': {
-			link: '#propdef-margin-left',
-			tests: ['auto', '10px', '5%'],
+		'border-bottom': {
+			link: '#border-shorthand-properties',
+			tests: borderShorthands,
 		},
-		'margin-top': {
-			link: '#propdef-margin-top',
-			tests: ['auto', '10px', '5%'],
+		'border-left': {
+			link: '#border-shorthand-properties',
+			tests: borderShorthands,
 		},
-		'margin-bottom': {
-			link: '#propdef-margin-bottom',
-			tests: ['auto', '10px', '5%'],
+		'border-top-color': {
+			link: '#border-color-properties',
+			tests: borderColors,
+		},
+		'border-right-color': {
+			link: '#border-color-properties',
+			tests: borderColors,
+		},
+		'border-bottom-color': {
+			link: '#border-color-properties',
+			tests: borderColors,
+		},
+		'border-left-color': {
+			link: '#border-color-properties',
+			tests: borderColors,
+		},
+		'border-top-style': {
+			link: '#border-style-properties',
+			tests: borderStyles,
+		},
+		'border-right-style': {
+			link: '#border-style-properties',
+			tests: borderStyles,
+		},
+		'border-bottom-style': {
+			link: '#border-style-properties',
+			tests: borderStyles,
+		},
+		'border-left-style': {
+			link: '#border-style-properties',
+			tests: borderStyles,
+		},
+		'border-top-width': {
+			link: '#border-width-properties',
+			tests: borderWidths,
+		},
+		'border-right-width': {
+			link: '#border-width-properties',
+			tests: borderWidths,
+		},
+		'border-bottom-width': {
+			link: '#border-width-properties',
+			tests: borderWidths,
+		},
+		'border-left-width': {
+			link: '#border-width-properties',
+			tests: borderWidths,
 		},
 		margin: {
 			link: '#propdef-margin',
-			tests: ['10px', '10px 5%', '10px 5px auto', '10px 5px auto 1em'],
+			tests: margin_1_4,
+		},
+		'margin-right': {
+			link: '#propdef-margin-right',
+			tests: margin,
+		},
+		'margin-left': {
+			link: '#propdef-margin-left',
+			tests: margin,
+		},
+		'margin-top': {
+			link: '#propdef-margin-top',
+			tests: margin,
+		},
+		'margin-bottom': {
+			link: '#propdef-margin-bottom',
+			tests: margin,
+		},
+
+		padding: {
+			link: '#padding-properties',
+			tests: padding_1_4,
 		},
 		'padding-top': {
 			link: '#padding-properties',
-			tests: ['10px', '5%'],
+			tests: padding,
 		},
 		'padding-right': {
 			link: '#padding-properties',
-			tests: ['10px', '5%'],
+			tests: padding,
 		},
 		'padding-bottom': {
 			link: '#padding-properties',
-			tests: ['10px', '5%'],
+			tests: padding,
 		},
 		'padding-left': {
 			link: '#padding-properties',
-			tests: ['10px', '5%'],
-		},
-		padding: {
-			link: '#padding-properties',
-			tests: ['10px', '10px 5%', '10px 5% 0.5em', '10px 5% 0.5em 0.8mm'],
+			tests: padding,
 		},
 	},
 };
