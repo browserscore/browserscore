a9fe2b956aaf836e6dd2a2f1760d7022ad58bf76
Lea Verou <lea@verou.me>
2025-08-30 17:31:14 +0300

    Update index.js

--- src/vue/components/index.js
+++ src/vue/components/index.js
@@ -3,3 +3,4 @@ export { default as CarbonAds } from './carbon-ads.js';
 export { default as SupportStatus } from './support-status/support-status.js';
 export { default as ColorSchemeToggle } from './color-scheme-toggle/color-scheme-toggle.js';
 export { default as Filter } from './filter/filter.js';
+export { default as WrapIf } from './wrap-if.js';
