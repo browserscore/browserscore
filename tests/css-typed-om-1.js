2ff42feaeff9ee5102b4015a9f8b413bed4321da
Lea Verou <lea@verou.me>
2025-09-01 18:05:01 +0300

    Update css-typed-om-1.js
    - Simplify interface tests
    - Simplify metadata

--- tests/css-typed-om-1.js
+++ tests/css-typed-om-1.js
@@ -1,27 +1,21 @@
 export default {
 	id: 'css-typed-om-1',
 	title: 'CSS Typed OM Level 1',
-	links: {
-		dev: 'css-typed-om-1',
-	},
+	link: 'css-typed-om-1',
 	group: 'houdini',
 	status: 'experimental',
 	interfaces: {
 		Element: {
-			links: {
-				dev: '#computed-stylepropertymapreadonly-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#computed-stylepropertymapreadonly-objects',
+			mdnGroup: 'DOM',
 			tests: ['computedStyleMap'],
 			interface: function() {
 				return document.body;
 			},
 		},
 		StylePropertyMapReadOnly: {
-			links: {
-				dev: '#the-stylepropertymap',
-				mdnGroup: 'DOM',
-			},
+			link: '#the-stylepropertymap',
+			mdnGroup: 'DOM',
 			tests: [
 				'get',
 				'getAll',
@@ -33,10 +27,8 @@ export default {
 			},
 		},
 		StylePropertyMap: {
-			links: {
-				dev: '#the-stylepropertymap',
-				mdnGroup: 'DOM',
-			},
+			link: '#the-stylepropertymap',
+			mdnGroup: 'DOM',
 			tests: [
 				'get',
 				'getAll',
@@ -52,20 +44,16 @@ export default {
 			},
 		},
 		CSSStyleValue: {
-			links: {
-				dev: '#stylevalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#stylevalue-objects',
+			mdnGroup: 'DOM',
 			tests: ['parse', 'parseAll'],
 			interface: function() {
 				return CSSStyleValue
 			},
 		},
 		CSSUnparsedValue: {
-			links: {
-				dev: '#unparsedvalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#unparsedvalue-objects',
+			mdnGroup: 'DOM',
 			tests: [
 				'length',
 				'entries',
@@ -73,38 +61,29 @@ export default {
 				'values',
 				'forEach',
 			],
-			required: ':root { --foo: 1px; }',
 			interface: function(style) {
 				return style.sheet.cssRules[0].styleMap.get('--foo');
 			},
 		},
 		CSSVariableReferenceValue: {
-			links: {
-				dev: '#unparsedvalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#unparsedvalue-objects',
+			mdnGroup: 'DOM',
 			tests: ['variable', 'fallback'],
-			required: ':root { --foo: 1px; margin-top: var(--foo, 2px); }',
 			interface: function(style) {
 				return style.sheet.cssRules[0].styleMap.get('margin-top')[0];
 			},
 		},
 		CSSKeywordValue: {
-			links: {
-				dev: '#keywordvalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#keywordvalue-objects',
+			mdnGroup: 'DOM',
 			tests: ['value'],
-			required: ':root { display: grid; }',
 			interface: function(style) {
 				return style.sheet.cssRules[0].styleMap.get('display');
 			},
 		},
 		CSSNumericValue: {
-			links: {
-				dev: '#numeric-value',
-				mdnGroup: 'DOM',
-			},
+			link: '#numeric-value',
+			mdnGroup: 'DOM',
 			tests: [
 				'add',
 				'sub',
@@ -117,16 +96,13 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { opacity: 1; }',
 			interface: function(style) {
 				return style.sheet.cssRules[0].styleMap.get('opacity');
 			},
 		},
 		CSSUnitValue: {
-			links: {
-				dev: '#simple-numeric',
-				mdnGroup: 'DOM',
-			},
+			link: '#simple-numeric',
+			mdnGroup: 'DOM',
 			tests: [
 				'value',
 				'unit',
@@ -141,16 +117,13 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { margin-top: 1px; }',
 			interface: function(style) {
 				return style.sheet.cssRules[0].styleMap.get('margin-top');
 			},
 		},
 		CSSMathValue: {
-			links: {
-				dev: '#complex-numeric',
-				mdnGroup: 'DOM',
-			},
+			link: '#complex-numeric',
+			mdnGroup: 'DOM',
 			tests: [
 				'operator',
 				'add',
@@ -164,16 +137,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { margin-top: calc(1vh + 10px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('margin-top');
-			},
 		},
 		CSSMathSum: {
-			links: {
-				dev: '#cssmathsum',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathsum',
+			mdnGroup: 'DOM',
 			tests: [
 				'values',
 				'operator',
@@ -188,16 +155,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { margin-top: calc(1vh + 10px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('margin-top');
-			},
 		},
 		CSSMathProduct: {
-			links: {
-				dev: '#cssmathproduct',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathproduct',
+			mdnGroup: 'DOM',
 			tests: [
 				'values',
 				'operator',
@@ -212,15 +173,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			interface: function(style) {
-				return new CSSMathProduct(2, 3);
-			},
 		},
 		CSSMathNegate: {
-			links: {
-				dev: '#cssmathnegate',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathnegate',
+			mdnGroup: 'DOM',
 			tests: [
 				'value',
 				'operator',
@@ -235,15 +191,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			interface: function(style) {
-				return new CSSMathNegate(3);
-			},
 		},
 		CSSMathInvert: {
-			links: {
-				dev: '#cssmathinvert',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathinvert',
+			mdnGroup: 'DOM',
 			tests: [
 				'value',
 				'operator',
@@ -258,15 +209,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			interface: function(style) {
-				return new CSSMathInvert(3);
-			},
 		},
 		CSSMathMin: {
-			links: {
-				dev: '#cssmathmin',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathmin',
+			mdnGroup: 'DOM',
 			tests: [
 				'values',
 				'operator',
@@ -281,16 +227,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { margin-top: min(20vh, 100px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('margin-top');
-			},
 		},
 		CSSMathMax: {
-			links: {
-				dev: '#cssmathmax',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathmax',
+			mdnGroup: 'DOM',
 			tests: [
 				'values',
 				'operator',
@@ -305,16 +245,10 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { margin-top: max(20vh, 400px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('margin-top');
-			},
 		},
 		CSSMathClamp: {
-			links: {
-				dev: '#cssmathclamp',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmathclamp',
+			mdnGroup: 'DOM',
 			tests: [
 				'lower',
 				'value',
@@ -331,27 +265,15 @@ export default {
 				'toSum',
 				'type',
 			],
-			required: ':root { margin-top: clamp(100px, 20vh, 400px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('margin-top');
-			},
 		},
 		CSSNumericArray: {
-			links: {
-				dev: '##cssnumericarray',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssnumericarray',
+			mdnGroup: 'DOM',
 			tests: ['length'],
-			required: ':root { margin-top: calc(1% + 10px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('margin-top').values;
-			},
 		},
 		CSS: {
-			links: {
-				dev: '#numeric-factory',
-				mdnGroup: 'DOM',
-			},
+			link: '#numeric-factory',
+			mdnGroup: 'DOM',
 			tests: [
 				'number',
 				'percent',
@@ -417,10 +339,8 @@ export default {
 			},
 		},
 		CSSTransformValue: {
-			links: {
-				dev: '#transformvalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#transformvalue-objects',
+			mdnGroup: 'DOM',
 			tests: [
 				'length',
 				'is2D',
@@ -430,211 +350,99 @@ export default {
 				'keys',
 				'values',
 			],
-			required: ':root { transform: translate(10px, 20px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform');
-			},
 		},
 		CSSTransformComponent: {
-			links: {
-				dev: '#csstransformcomponent',
-				mdnGroup: 'DOM',
-			},
+			link: '#csstransformcomponent',
+			mdnGroup: 'DOM',
 			tests: ['is2D', 'toMatrix'],
-			required: ':root { transform: translate(10px, 20px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSTranslate: {
-			links: {
-				dev: '#csstranslate',
-				mdnGroup: 'DOM',
-			},
+			link: '#csstranslate',
+			mdnGroup: 'DOM',
 			tests: ['x', 'y', 'z'],
-			required: ':root { transform: translate(10px, 20px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSRotate: {
-			links: {
-				dev: '#cssrotate',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssrotate',
+			mdnGroup: 'DOM',
 			tests: ['x', 'y', 'z', 'angle'],
-			required: ':root { transform: rotate(10deg); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSScale: {
-			links: {
-				dev: '#cssscale',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssscale',
+			mdnGroup: 'DOM',
 			tests: ['x', 'y', 'z'],
-			required: ':root { transform: scale(2); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSSkew: {
-			links: {
-				dev: '#cssskew',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssskew',
+			mdnGroup: 'DOM',
 			tests: ['ax', 'ay'],
-			required: ':root { transform: skew(10deg, 20deg); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSSkewX: {
-			links: {
-				dev: '#cssskewx',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssskewx',
+			mdnGroup: 'DOM',
 			tests: ['ax'],
-			required: ':root { transform: skewX(10deg); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSSkewY: {
-			links: {
-				dev: '#cssskewy',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssskewy',
+			mdnGroup: 'DOM',
 			tests: ['ay'],
-			required: ':root { transform: skewY(10deg); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSPerspective: {
-			links: {
-				dev: '#cssperspective',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssperspective',
+			mdnGroup: 'DOM',
 			tests: ['length'],
-			required: ':root { transform: perspective(10px); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSMatrixComponent: {
-			links: {
-				dev: '#cssmatrixcomponent',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssmatrixcomponent',
+			mdnGroup: 'DOM',
 			tests: ['matrix'],
-			required: ':root { transform: matrix(1, 0, 0, 1, 0, 0); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('transform')[0];
-			},
 		},
 		CSSImageValue: {
-			links: {
-				dev: '#imagevalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#imagevalue-objects',
+			mdnGroup: 'DOM',
 			tests: ['CSSImageValue'],
 		},
 		CSSColorValue: {
-			links: {
-				dev: '#colorvalue-objects',
-				mdnGroup: 'DOM',
-			},
+			link: '#colorvalue-objects',
+			mdnGroup: 'DOM',
 		 	tests: ['CSSColorValue'],
 		},
 		CSSRGB: {
-			links: {
-				dev: '#cssrgb',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssrgb',
+			mdnGroup: 'DOM',
 		 	tests: ['r', 'g', 'b', 'alpha'],
-			required: ':root { color: rgb(255, 0, 0); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 		CSSHSL: {
-			links: {
-				dev: '#csshsl',
-				mdnGroup: 'DOM',
-			},
+			link: '#csshsl',
+			mdnGroup: 'DOM',
 		 	tests: ['h', 's', 'l', 'alpha'],
-			required: ':root { color: hsl(0, 100%, 50%); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 		CSSHWB: {
-			links: {
-				dev: '#csshwb',
-				mdnGroup: 'DOM',
-			},
+			link: '#csshwb',
+			mdnGroup: 'DOM',
 		 	tests: ['h', 'w', 'b', 'alpha'],
-			required: ':root { color: hwb(0, 0%, 0%); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 		CSSLab: {
-			links: {
-				dev: '#csslab',
-				mdnGroup: 'DOM',
-			},
+			link: '#csslab',
 		 	tests: ['l', 'a', 'b', 'alpha'],
-			required: ':root { color: lab(0%, 0%, 0%); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 		CSSLCH: {
-			links: {
-				dev: '#csslch',
-				mdnGroup: 'DOM',
-			},
-		 	tests: ['l', 'c', 'h', 'alpha'],
-			required: ':root { color: lch(0%, 0%, 0deg); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
+			link: '#csslch',
+			mdnGroup: 'DOM',
 		},
 		CSSOKLab: {
-			links: {
-				dev: '#cssoklab',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssoklab',
+			mdnGroup: 'DOM',
 		 	tests: ['l', 'a', 'b', 'alpha'],
-			required: ':root { color: oklab(0%, 0%, 0%); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 		CSSOKLCH: {
-			links: {
-				dev: '#cssoklch',
-				mdnGroup: 'DOM',
-			},
+			link: '#cssoklch',
+			mdnGroup: 'DOM',
 		 	tests: ['l', 'c', 'h', 'alpha'],
-			required: ':root { color: oklch(0%, 0%, 0deg); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 		CSSColor: {
-			links: {
-				dev: '#csscolor',
-				mdnGroup: 'DOM',
-			},
+			link: '#csscolor',
+			mdnGroup: 'DOM',
 			tests: ['colorSpace', 'channels', 'alpha'],
-			required: ':root { color: color(display-p3 1 0 0); }',
-			interface: function(style) {
-				return style.sheet.cssRules[0].styleMap.get('color');
-			},
 		},
 	},
 };
