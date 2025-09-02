818753deb58ef4384e467c2839a6f71586473da7
Lea Verou <lea@verou.me>
2025-09-02 17:25:14 +0300

    Ditch last traces of the `required` property
    Good riddance

--- tests/css-properties-values-api-1.js
+++ tests/css-properties-values-api-1.js
@@ -4,45 +4,6 @@ export default {
 	link: 'css-properties-values-api-1',
 	group: 'houdini',
 	status: 'experimental',
-	xdescriptors: {
-		'@property --foo/syntax': {
-			link: '#the-syntax-descriptor',
-			required: {
-				"'x | y'": {
-					descriptor: "inherits: false; initial-value: x",
-				},
-				"'<length>'": {
-					descriptor: "inherits: false; initial-value: 100px",
-				},
-				"'<color>'": {
-					descriptor: "inherits: false; initial-value: red",
-				},
-			},
-			tests: [
-				"'x | y'",
-				"'<length>'",
-				"'<color>'",
-			],
-		},
-		'@property --foo/inherits': {
-			link: '#inherits-descriptor',
-			required: {
-				'*': {
-					descriptor: "syntax: '<color>'; initial-value: red",
-				},
-			},
-			tests: ['true', 'false'],
-		},
-		'@property --foo/initial-value': {
-			link: '#initial-value-descriptor',
-			required: {
-				'*': {
-					descriptor: "syntax: '<color>'; inherits: false",
-				},
-			},
-			tests: ['blue', '#00f', 'rgb(0, 0, 255)'],
-		},
-	},
 	atrules: {
 		'@property': {
 			link: '#at-property-rule',
