af6f4b0f508a942c7e0aae70f526a1a0a00b3577
Dmitry Sharabin <dmitrysharabin@gmail.com>
2025-09-09 13:53:31 +0200

    Pointer Events Level 1

--- features/pointerevents1.js
+++ features/pointerevents1.js
@@ -1,15 +1,11 @@
 export default {
 	id: 'pointerevents-1',
 	title: 'Pointer Events Level 1',
-	links: {
-		tr: 'pointerevents1',
-	},
+	specLink: 'pointerevents1',
 	status: 'stable',
 	properties: {
 		'touch-action': {
-			links: {
-				tr: '#the-touch-action-css-property',
-			},
+			specLink: '#the-touch-action-css-property',
 			tests: ['auto', 'none', 'pan-x', 'pan-y', 'pan-x pan-y', 'manipulation'],
 		},
 	},
